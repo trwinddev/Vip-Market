00923a90c0716d0fec871a28d9c12ad3e1aa02a6
trwinddev <ngotrongphong.dev@gmail.com>
2023-09-20 23:52:21 +0700

    handle reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -6,20 +6,12 @@
                 <div class="card-body p-md-5">
                     <div class="row justify-content-center">
                         <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                            <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login</p>
-                            <form action="{{ route('login') }}" method="post" class="mx-1 mx-md-4">
+                            <p class="text-center h2 fw-bold mb-5 mx-1 mx-md-4 mt-4">Create New Password</p>
+                            <form action="{{ route('password.update') }}" method="post" class="mx-1 mx-md-4">
                                 @csrf
                                 <div class="d-flex flex-row align-items-center mb-4">
-                                    <input type="email" name="email"
-                                        class="form-control @error('email') is-invalid @enderror"
-                                        placeholder="Email address" />
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>
-                                                {{ $message }}
-                                            </strong>
-                                        </span>
-                                    @enderror
+                                    <input type="email" name="email" class="form-control" value="{{ request('email') }}"
+                                        readonly />
                                 </div>
                                 <div class="d-flex flex-row align-items-center mb-4">
                                     <input type="password" name="password"
@@ -33,19 +25,16 @@
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="form-group">
-                                    <div class="form-check d-flex justify-content-between">
-                                        <input type="checkbox" class="form-check-input" name="remember" id="remember">
-                                        <label class="form-check-label" for="remember">Remember me</label>
-                                        <p><a href="{{ route('password.request') }}">Forgot password?</a></p>
-                                    </div>
+                                <div class="d-flex flex-row align-items-center mb-4">
+                                    <input type="password" name="password_confirmation"
+                                        class="form-control @error('password_confirmation') is-invalid @enderror"
+                                        placeholder="Confirm password" />
+                                    @error('password_confirmation')
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-danger w-100 mb-3">Login</button>
-                                    <a class="btn"
-                                        style="background: #3B5499; color: #ffffff; width: 100%; text-align: center; display: block; border-radius:3px;">
-                                        Login with Facebook
-                                    </a>
+                                    <input type="hidden" name="token" value="{{ request()->route('token') }}">
+                                    <button type="submit" class="btn btn-danger w-100">Reset Password</button>
                                 </div>
                             </form>
                         </div>
