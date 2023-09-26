42fa056349660b83a821d9db877f85b265337197
trwinddev <ngotrongphong.dev@gmail.com>
2023-09-26 14:59:18 +0700

    change ui sidebar category

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -12,14 +12,14 @@
                 <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
                     aria-controls="ui-basic">
                     <i class="mdi mdi-circle-outline menu-icon"></i>
-                    <span class="menu-title">UI Elements</span>
+                    <span class="menu-title">Category</span>
                     <i class="menu-arrow"></i>
                 </a>
                 <div class="collapse" id="ui-basic">
                     <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                        <li class="nav-item"> <a class="nav-link"
-                                href="pages/ui-features/typography.html">Typography</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('category.create') }}">Add</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('category.index') }}">Manage</a>
+                        </li>
                     </ul>
                 </div>
             </li>
