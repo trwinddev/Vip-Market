df3a42574a36b83cfa7423801284a6efc371826d
trwinddev <ngotrongphong.dev@gmail.com>
2023-09-26 14:27:19 +0700

    handle delete category

--- resources/views/backend/category/index.blade.php
+++ resources/views/backend/category/index.blade.php
@@ -30,9 +30,51 @@
                                                     </a>
                                                 </td>
                                                 <td>
-                                                    <button class="btn btn-danger">
-                                                        <i class="mdi mdi-delete"></i>
-                                                    </button>
+                                                    <form action="{{ route('category.destroy', $category->id) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <!-- Button trigger modal -->
+                                                        <button type="button" class="btn btn-danger" data-toggle="modal"
+                                                            data-target="#exampleModal{{ $category->id }}">
+                                                            <i class="mdi mdi-delete"></i>
+                                                        </button>
+
+                                                        <!-- Modal -->
+                                                        <div class="modal fade" id="exampleModal{{ $category->id }}"
+                                                            tabindex="-1" aria-labelledby="exampleModalLabel"
+                                                            aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <h5 class="modal-title" id="exampleModalLabel">
+                                                                            Delete Category</h5>
+                                                                        <button type="button" class="close"
+                                                                            data-dismiss="modal" aria-label="Close">
+                                                                            <span aria-hidden="true">&times;</span>
+                                                                        </button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        If you delete a category will be gone forever. Are
+                                                                        you sure you want to proceed?
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="button" class="btn btn-secondary"
+                                                                            data-dismiss="modal">Cancel</button>
+                                                                        <a
+                                                                            href="{{ route('category.destroy', [$category->id]) }}">
+                                                                            <button class="btn btn-danger">
+                                                                                Delete
+                                                                            </button>
+                                                                        </a>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        {{-- <button class="btn btn-danger">
+                                                            <i class="mdi mdi-delete"></i>
+                                                        </button> --}}
+                                                    </form>
                                                 </td>
                                             </tr>
                                         @empty
