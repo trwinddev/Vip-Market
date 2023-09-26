ef204b9afcc983fe8d596fb869e2cd26b9ca75dd
trwinddev <ngotrongphong.dev@gmail.com>
2023-09-26 10:59:11 +0700

    handle get all category

---
+++ resources/views/backend/category/index.blade.php
@@ -0,0 +1,49 @@
+@extends('backend.layouts.master')
+@section('content')
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <h4>Manage Category</h4>
+            <div class="row justify-content-center">
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Image</th>
+                                            <th>Name</th>
+                                            <th>Edit</th>
+                                            <th>Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse($categories as $category)
+                                            {{-- <?php
+                                            dd(Storage::url($category->image));
+                                            ?> --}}
+                                            <tr>
+                                                <td><img src="{{ Storage::url($category->image) }}"></td>
+                                                <td>{{ $category->name }}</td>
+                                                <td>
+                                                    <button class="btn btn-info">
+                                                        <i class="mdi mdi-table-edit"></i>
+                                                    </button>
+                                                </td>
+                                                <td>
+                                                    <button class="btn btn-danger">
+                                                        <i class="mdi mdi-delete"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <td>No category to display</td>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endsection
