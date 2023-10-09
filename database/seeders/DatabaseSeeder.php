e4b422c759c69394708ecd033a7bc56c71d2c166
trwinddev <ngotrongphong.dev@gmail.com>
2023-10-09 23:22:31 +0700

    migrate and seeder city, country, state

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(CountryTableSeeder::class);
+        $this->call(StateTableSeeder::class);
+        $this->call(CityTableSeeder::class);
     }
 }
