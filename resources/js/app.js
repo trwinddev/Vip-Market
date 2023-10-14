c0670a080b3017701226fb1b1725388dc665d1d7
trwinddev <ngotrongphong.dev@gmail.com>
2023-10-14 21:57:18 +0700

    handle preview image and make api category

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue");
+window.axios = require("axios");
+
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.component(
+    "image-preview",
+    require("./components/imagepreview/FeatureImage.vue").default
+);
+Vue.component(
+    "first-image",
+    require("./components/imagepreview/FirstImage.vue").default
+);
+Vue.component(
+    "second-image",
+    require("./components/imagepreview/SecondImage.vue").default
+);
+
+const app = new Vue({
+    el: "#app",
+});
---
+++ resources/js/components/imagepreview/FeatureImage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input
+            type="file"
+            name="feature_image"
+            @change="onFileChange"
+            class="custom-file-upload"
+            accept="image/*"
+        />
+        <div id="preview">
+            <img v-if="url" :src="url" width="100" height="60" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            url: "",
+        };
+    },
+    methods: {
+        onFileChange(e) {
+            const file = e.target.files[0];
+            this.url = URL.createObjectURL(file);
+        },
+    },
+};
+</script>
+
+<style></style>
